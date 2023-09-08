822be0df96074c40287eedd2f1da43c54395741b
jordanleewei <jordanleewei@gmail.com>
2023-09-08 23:24:14 +0800

    added clickable link to return to previous page

--- src/components/ProfileImagePage/ProfileImagePage.jsx
+++ src/components/ProfileImagePage/ProfileImagePage.jsx
@@ -2,11 +2,13 @@
 
 import React from 'react';
 import './ProfileImagePage.css';  // Import the CSS file
+import { Link } from 'react-router-dom';
 
 function ProfileImagePage() {
   return (
     <div className="profile-image-page">
       <img src="/TikTok User.png" alt="Profile" className="full-profile-image" />
+      <Link to="/" className="clickable-box"></Link>
     </div>
   );
 }
